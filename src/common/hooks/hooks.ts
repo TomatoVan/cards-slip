3b108fbc2eb52668c2395de9a3331da871318cb4
Leonid Kovzel <kovzel.leo@gmail.com>
2022-08-26 14:15:53 +0300

    Updates

--- src/common/hooks/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-
-import { AppStateType } from '../../app/store';
-import { ThunkDispatchType } from '../types/types';
-
-export const useAppDispatch: () => ThunkDispatchType = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector;
