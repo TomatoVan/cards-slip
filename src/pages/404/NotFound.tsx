8f1d8cbf580229ad02917e1543ee01acf9b40e49
Leonid Kovzel <kovzel.leo@gmail.com>
2022-09-04 16:09:23 +0300

    refactor: add content to  404 page

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
+import error404 from '../../assets/img/404.svg';
+import { Button } from '../../components/button/Button';
+
 export const NotFound = () => {
-  return <div>404</div>;
+  const navigate = useNavigate();
+
+  const goBackHandler = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="notFound">
+      <div className="notFound__container">
+        <div className="notFound__elems">
+          <div className="notFound__title">Oops!</div>
+          <div className="notFound__descr">Sorry! Page not found!</div>
+          <Button callBack={goBackHandler} title="Back to home page" submit={false} />
+        </div>
+        <img src={error404} alt="error" />
+      </div>
+    </div>
+  );
 };
