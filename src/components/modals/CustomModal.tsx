45ce910d41c8a78dcf7328c0b5235988352ebf8e
Leonid Kovzel <kovzel.leo@gmail.com>
2022-09-04 19:27:03 +0300

    refactor: custom modal styles to main.scss

--- src/components/modals/CustomModal.tsx
+++ src/components/modals/CustomModal.tsx
@@ -1,48 +1,41 @@
 import React, { ReactNode } from 'react';
 
 import CloseIcon from '@mui/icons-material/Close';
-import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 3,
-  borderRadius: 2,
-};
 
 type PropsType = {
   children: ReactNode;
   open: boolean;
   handleClose: () => void;
   title: string;
+  clearCoverCallback?: () => void;
 };
 
 export const CustomModal = React.memo(
-  ({ children, open, handleClose, title }: PropsType) => {
+  ({ children, open, handleClose, title, clearCoverCallback }: PropsType) => {
+    const onCloseHandler = () => {
+      if (clearCoverCallback) {
+        clearCoverCallback();
+      }
+      handleClose();
+    };
+
     return (
       <div>
         <Modal
           open={open}
-          onClose={handleClose}
+          onClose={onCloseHandler}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box sx={style}>
-            <div className="modals">
-              <div className="modals modals__title">
-                <div className="cut">{title}</div>
-                <CloseIcon onClick={() => handleClose()} />
-              </div>
-              <hr className="modals modals__hr" />
-              {children}
+          <div className="modals">
+            <div className="modals__title">
+              <div className="cut">{title}</div>
+              <CloseIcon onClick={onCloseHandler} />
             </div>
-          </Box>
+            <hr className="modals__hr" />
+            {children}
+          </div>
         </Modal>
       </div>
     );
